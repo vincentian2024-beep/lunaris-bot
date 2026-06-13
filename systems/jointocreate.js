24bc79708861b2a76dcadfb4646eeb468f1f8945
vincentian2024-beep <vincentian2024@gmail.com>
2026-06-14 06:36:08 +0800

    Update jointocreate.js

--- systems/jointocreate.js
+++ systems/jointocreate.js
@@ -1,1 +1,32 @@
+import fs from "fs";
 
+const JTC_CHANNEL = "1514875884975423518";
+const VC_CATEGORY = "1514630686630346945";
+
+const DATA_FILE = "./data/voice.json";
+
+function loadData() {
+  return JSON.parse(
+    fs.readFileSync(DATA_FILE, "utf8")
+  );
+}
+
+function saveData(data) {
+  fs.writeFileSync(
+    DATA_FILE,
+    JSON.stringify(data, null, 2)
+  );
+}
+
+export async function handleJoinToCreate(
+  oldState,
+  newState
+) {
+
+}
+
+export async function handleVCButtons(
+  interaction
+) {
+
+}
