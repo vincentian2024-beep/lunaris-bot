b8bc782d51d7c636d1a51e5cb54cade4f3a13dda
vincentian2024-beep <vincentian2024@gmail.com>
2026-06-15 00:08:57 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,39 @@
+import { EmbedBuilder } from "discord.js";
+
 export async function pingCommand(message) {
-  await message.reply("🏓 Pong!");
+  const sent = await message.reply(
+    "🏓 Pinging..."
+  );
+
+  const ping =
+    sent.createdTimestamp -
+    message.createdTimestamp;
+
+  const embed =
+    new EmbedBuilder()
+      .setColor("#a855f7")
+      .setTitle("🏓 Lunaris Ping")
+      .addFields(
+        {
+          name: "⚡ Latency",
+          value: `${ping}ms`,
+          inline: true
+        },
+        {
+          name: "🤖 API",
+          value: `${Math.round(
+            message.client.ws.ping
+          )}ms`,
+          inline: true
+        }
+      )
+      .setFooter({
+        text: "Lunaris Craft"
+      })
+      .setTimestamp();
+
+  await sent.edit({
+    content: "",
+    embeds: [embed]
+  });
 }
