5f28660f937a4ac2b7354fe69aa00919b2201f1b
vincentian2024-beep <vincentian2024@gmail.com>
2026-06-15 18:25:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -265,25 +265,6 @@ if (
   });
 }
 
-  const data = JSON.parse(
-    fs.readFileSync(
-      "./data/voice.json",
-      "utf8"
-    )
-  );
-
-  if (
-    !data[channel.id] ||
-    data[channel.id].owner !==
-      interaction.user.id
-  ) {
-    return interaction.reply({
-      content:
-        "❌ Only the VC owner can rename the VC.",
-      ephemeral: true
-    });
-  }
-
   const name =
     interaction.fields.getTextInputValue(
       "new_name"
@@ -297,25 +278,6 @@ if (
     ephemeral: true
   });
 }
-   
-  const data = JSON.parse(
-    fs.readFileSync(
-      "./data/voice.json",
-      "utf8"
-    )
-  );
-
-  if (
-    !data[channel.id] ||
-    data[channel.id].owner !==
-      interaction.user.id
-  ) {
-    return interaction.reply({
-      content:
-        "❌ Only the VC owner can change the limit.",
-      ephemeral: true
-    });
-  }
 
   const limit = parseInt(
     interaction.fields.getTextInputValue(
